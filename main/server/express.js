91b4dc87b36839ee57c9106cbb58d0e9ff5d694d
QD@#er1m <clintf3000@gmail.com>
2020-09-05 14:06:26 +0300

    v

--- main/server/express.js
+++ main/server/express.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
 	console.log("Run \"Express\" server.");
+	app.get("/", (req, res) => {
+		res.redirect("./chat");
+	});
 	app.get("/chat", (req, res) => {
 		res.sendFile(tool.string.pathFix(`${__dirname.substring(0, __dirname.length - 7)}/index.html`));
 	});
